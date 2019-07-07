63f284798509f05613dd1d742af89f634c024a41
cmilu <shuo.liu@linecorp.com>
2019-07-07 22:23:16 +0900

    enums

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import { RouterAdmin } from './routes'
+import { ApiError } from './var'
 
 const app = express()
 
@@ -8,18 +9,18 @@ app.use(bodyParser.json())
 app.use('/admin/v1', RouterAdmin)
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err: { code: ApiError }, _, res, next) {
   if (res.headersSent) {
     return next(err)
   }
   switch (err.code) {
-    case 'request.malformed':
+    case ApiError.RequestMalformed:
       res.status(400)
       break
-    case 'auth.required':
+    case ApiError.AuthRequired:
       res.status(401)
       break
-    case 'auth.forbidden':
+    case ApiError.AuthForbidden:
       res.status(403)
       break
     default:
